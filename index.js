d75bc61364fec7587f59a32776bf654f65f94f2d
Brett Morriss <BrettMorriss@hotmail.com>
2015-02-22 15:20:22 -0800

    add routes

--- index.js
+++ index.js
@@ -15,6 +15,34 @@ server.route({
        reply('hello world');
     }
 });
+server.route({
+    method: 'GET',
+    path:'/hapi', 
+    handler: function (request, reply) {
+       reply('<a href="http://hapijs.com/">hapijs.com</a>');
+       //reply('http://hapijs.com/');
+    }
+}
+);
+
+/**
+ *
+ *  This works like 'all others'
+ *
+ *  Note that we URI encode the name parameter, this is to prevent content
+ *  injection attacks. Remember, it's never a good idea to render user
+ *  provided data without output encoding it first!
+ *
+ **/
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
 
 // Start the server
-server.start();
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
