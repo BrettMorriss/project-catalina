b4db36c5ed703af3aeded1849b22e7cc198ef9a9
Brett Morriss <BrettMorriss@hotmail.com>
2015-02-22 15:25:19 -0800

    add good logging

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+var Good = require('good'); 
 
 // Create a server with a host and port
 var server = new Hapi.Server();
@@ -40,6 +41,24 @@ server.route({
     handler: function (request, reply) {
         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
     }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            args:[{ log: '*', response: '*' }]
+        }]
+    }
+}, function (err) {
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start(function () {
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
 });
 
 // Start the server
